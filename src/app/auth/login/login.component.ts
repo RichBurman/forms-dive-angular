cf1ec4d692fb630f32fa550125d2e0b9a34d5987
Richard Burman <richard.burman@harperjames.co.uk>
2025-10-24 15:56:49 +0100

    refactor login component to implement form value change tracking and local storage saving

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +16,26 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  onSubmit(formData: NgForm) {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
 
-    if(formData.form.invalid) {
+  constructor() {
+    afterNextRender(() => {
+      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
+        next: (value) =>
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: value.email })
+          ),
+      });
+
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
+  onSubmit(formData: NgForm) {
+    if (formData.form.invalid) {
       console.log('Form is invalid');
-      return
+      return;
     }
     const enteredEmail = formData.form.value.email;
     const enteredPassword = formData.form.value.password;
@@ -21,5 +43,7 @@ export class LoginComponent {
     console.log(formData);
     console.log(enteredEmail);
     console.log(enteredPassword);
+
+    formData.form.reset();
   }
 }
