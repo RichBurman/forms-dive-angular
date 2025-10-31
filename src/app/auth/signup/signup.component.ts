9aa519e6557f58b118f6f7ca7bae496d68e4e199
Richard Burman <richard.burman@harperjames.co.uk>
2025-10-31 14:35:31 +0000

    refactor signup component to hook up email and password

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  ɵInternalFormsSharedModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+
+  onSubmit() {
+    const enteredEmail = this.form.controls.email.value;
+    const enteredPassword = this.form.controls.password.value;
+    console.log(this.form);
+    console.log(enteredEmail, enteredPassword)
+  }
+
+  onReset() {
+    this.form.reset()
+  }
+}
