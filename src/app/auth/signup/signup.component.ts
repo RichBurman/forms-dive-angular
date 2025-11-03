837d7ea539b12d819a79f058173d4b456a00d3d7
Richard Burman <richard.burman@harperjames.co.uk>
2025-11-03 14:30:57 +0000

    refactor signup component to add form controls for confirm password, first name, last name, street, number, postal code, city, role, and agreement

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -6,6 +6,7 @@ import {
   Validators,
   ɵInternalFormsSharedModule,
 } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -17,21 +18,32 @@ import {
 export class SignupComponent {
   form = new FormGroup({
     email: new FormControl('', [Validators.email, Validators.required]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(6),
-    ]),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+    confirmPassword: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+    firstName: new FormControl('', { validators: [Validators.required] }),
+    lastName: new FormControl('', { validators: [Validators.required] }),
+    street: new FormControl('', { validators: [Validators.required] }),
+    number: new FormControl('', { validators: [Validators.required] }),
+    postalCode: new FormControl('', { validators: [Validators.required] }),
+    city: new FormControl('', { validators: [Validators.required] }),
+    role: new FormControl<
+      'student' | 'teacher' | 'employee' | 'founder' | 'other'
+    >('student', { validators: [Validators.required] }),
+    agree: new FormControl(false, { validators: [Validators.required] }),
   });
-
 
   onSubmit() {
     const enteredEmail = this.form.controls.email.value;
     const enteredPassword = this.form.controls.password.value;
     console.log(this.form);
-    console.log(enteredEmail, enteredPassword)
+    console.log(enteredEmail, enteredPassword);
   }
 
   onReset() {
-    this.form.reset()
+    this.form.reset();
   }
 }
